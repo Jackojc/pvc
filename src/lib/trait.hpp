11aa694081f3c7982ef34b93ce9ef602ee9d6029
Jack Clarke <jackojc@gmail.com>
2021-09-13 18:03:26 +0100

    New type traits
    - is_array
    - add_rvalue_reference
    - conditional (if/else)
    - conjunction (logical and)
    - disjunction (logical or)
    - is_void
    - is_convertible (is T convertible to some other T)
    - equivalence (check if variadic pack types are convertible to T)
    - parity (check if variadic pack types are all the same)
    - first (get first type from variadic pack)
    - last (get last type from variadic pack)
    - is_detected (check if T supports functionality like operators/methods or free functions)
    - is_specialisation (check if T is a specialisation of a wrapper type like vec<T>)

--- src/lib/trait.hpp
+++ src/lib/trait.hpp
@@ -7,7 +7,7 @@ namespace br {
 
 	// https://en.cppreference.com/w/cpp/types/integral_constant
 	// Integral constants and true/false types.
-	template <class T, T v>
+	template <typename T, T v>
 	struct integral_constant {
 		static constexpr T value = v;
 		using value_type = T;
@@ -120,6 +120,192 @@ namespace br {
 
 	template <typename T> constexpr bool is_signed_v = is_signed<T>::value;
 	template <typename T> constexpr bool is_unsigned_v = is_unsigned<T>::value;
+
+
+	// is_array
+	template <typename T>
+	struct is_array: false_type {};
+
+	template <typename T>
+	struct is_array<T[]>: true_type {};
+
+	template <typename T, size_t N>
+	struct is_array<T[N]>: true_type {};
+
+
+	template <typename T>
+	constexpr bool is_array_v = is_array<T>::value;
+
+
+	// add_rvalue_reference
+	template <typename T>
+	struct type_identity { using type = T; };
+
+	template <typename T>
+	auto try_add_rvalue_reference(int) -> type_identity<T&&>;
+
+	template <typename T>
+	auto try_add_rvalue_reference(...) -> type_identity<T>;
+
+	template <typename T>
+	struct add_rvalue_reference: decltype(try_add_rvalue_reference<T>(0)) {};
+
+
+	// declval
+	template <typename T>
+	typename add_rvalue_reference<T>::type declval();
+
+
+	// conditional
+	template <bool B, typename T, typename F>
+	struct conditional { using type = T; };
+
+	template <typename T, typename F>
+	struct conditional<false, T, F> { using type = F; };
+
+	template <bool B, typename T, typename F>
+	using conditional_t = typename conditional<B, T, F>::type;
+
+
+	// conjunction (logical and)
+	template <typename...> struct conjunction: true_type { };
+
+	template <typename B1> struct conjunction<B1>: B1 { };
+	template <typename B1, typename... Bn>
+
+	struct conjunction<B1, Bn...>: conditional_t<bool(B1::value), conjunction<Bn...>, B1> {};
+
+	template <typename... B>
+	constexpr bool conjunction_v = conjunction<B...>::value;
+
+
+	// disjunction (logical or)
+	template <typename...> struct disjunction: false_type { };
+
+	template <typename B1> struct disjunction<B1>: B1 { };
+	template <typename B1, typename... Bn>
+
+	struct disjunction<B1, Bn...>: conditional_t<bool(B1::value), B1, disjunction<Bn...>>  { };
+
+	template <typename... B>
+	constexpr bool disjunction_v = disjunction<B...>::value;
+
+
+	// is_void
+	template <typename T>
+	struct is_void: is_same<void, typename remove_cv<T>::type> {};
+
+	template <typename T>
+	constexpr bool is_void_v = is_void<T>::value;
+
+
+	// is_convertible
+	namespace detail {
+		template <typename T>
+		constexpr auto test_returnable(int) -> decltype(
+			void(static_cast<T(*)()>(nullptr)), true_type{}
+		);
+
+		template <typename>
+		constexpr auto test_returnable(...) -> false_type;
+
+		template <typename From, typename To>
+		constexpr auto test_implicitly_convertible(int) -> decltype(
+			void(declval<void(&)(To)>()(declval<From>())), true_type{}
+		);
+
+		template <typename, typename>
+		constexpr auto test_implicitly_convertible(...) -> false_type;
+	}
+
+	template <typename From, typename To>
+	struct is_convertible: integral_constant<bool,
+		(decltype(detail::test_returnable<To>(0))::value &&
+		decltype(detail::test_implicitly_convertible<From, To>(0))::value) ||
+		(is_void<From>::value && is_void<To>::value)
+	> {};
+
+	template <typename From, typename To>
+	constexpr bool is_convertible_v = is_convertible<From, To>::value;
+
+
+	// equivalence (Ts are convertible to T)
+	template <typename T, typename... Ts>
+	struct equivalence: conjunction<is_convertible<Ts, T>...> {};
+
+	template <typename T, typename... Ts>
+	constexpr bool equivalence_v = equivalence<T, Ts...>::value;
+
+
+	// parity (Ts are the same as T)
+	template <typename T, typename... Ts>
+	struct parity: conjunction<is_same<T, Ts>...> {};
+
+	template <typename T, typename... Ts>
+	constexpr bool parity_v = parity<T, Ts...>::value;
+
+
+	// first/last (first and last types in variadic pack)
+	template <typename T, typename... Ts>
+	struct first { using type = T; };
+
+	template <typename T, typename... Ts>
+	struct last { using type = typename last<Ts...>::type; };
+
+
+	template <typename... Ts>
+	using first_t = typename first<Ts...>::type;
+
+	template <typename... Ts>
+	using last_t = typename last<Ts...>::type;
+
+
+	// See http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2015/n4502.pdf.
+	template <typename...>
+	using void_t = void;
+
+
+	template <typename Default, typename AlwaysVoid, template<typename...> typename Op, typename... Args>
+	struct detector {
+		using value_t = false_type;
+		using type = Default;
+	};
+
+	template <typename Default, template<typename...> typename Op, typename... Args>
+	struct detector<Default, void_t<Op<Args...>>, Op, Args...> {
+		using value_t = true_type;
+		using type = Op<Args...>;
+	};
+
+	struct nonesuch {
+		nonesuch() = delete;
+		~nonesuch() = delete;
+		nonesuch(nonesuch const&) = delete;
+		void operator=(nonesuch const&) = delete;
+	};
+
+	template <template<typename...> typename Op, typename... Args>
+	using is_detected = typename detector<nonesuch, void, Op, Args...>::value_t;
+
+	template <template<typename...> typename Op, typename... Args>
+	constexpr bool is_detected_v = is_detected<Op, Args...>::value;
+
+	template <template<typename...> typename Op, typename... Args>
+	using detected_t = typename detector<nonesuch, void, Op, Args...>::type;
+
+	template <typename Default, template<typename...> typename Op, typename... Args>
+	using detected_or = detector<Default, void, Op, Args...>;
+
+
+	// is_specialisation
+	template <typename Test, template<typename...> class Ref>
+	struct is_specialisation: false_type {};
+
+	template <template<typename...> class Ref, typename... Args>
+	struct is_specialisation<Ref<Args...>, Ref>: true_type {};
+
+	template <typename Test, template<typename...> class Ref>
+	constexpr bool is_specialisation_v = is_specialisation<Test, Ref>::value;
 
 }
 
