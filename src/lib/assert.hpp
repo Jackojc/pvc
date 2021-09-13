c528cc608f68053e565d0fbd0534721680a610d1
Jack Clarke <jackojc@gmail.com>
2021-09-13 17:50:57 +0100

    Remove internal assert header
    Removed the internal assert header in favour of just using C IO
    instead of our custom IO.
    
    The reason we needed this header originally was due to the issue of
    circular dependence on our IO routines but I think its better to just
    use C IO for all of the asserts.

--- src/lib/assert.hpp
+++ src/lib/assert.hpp
@@ -1,75 +1,50 @@
 #ifndef BR_ASSERT_H
 #define BR_ASSERT_H
 
+#include <cstdio>
+
 #include <lib/def.hpp>
-#include <lib/io.hpp>
+#include <lib/exit.hpp>
 
 namespace br {
 
 	// BR_ENABLE_ASSERT  => Enable assertions even when NDEBUG is set.
 	// BR_DISABLE_ASSERT => Disable assertions explicitly.
 
-	#ifdef NDEBUG
-		#ifndef BR_ENABLE_ASSERT
-			#define BR_DISABLE_ASSERT
-		#endif
+	#if defined(NDEBUG) && !defined(BR_ENABLE_ASSERT)
+		#define BR_DISABLE_ASSERT
 	#endif
-
 
 	#define BR_UNIMPLEMENTED() \
 		do { \
-			br::halt("[{}:{}] unimplemented!", __FILE__, BR_STR(__LINE__)); \
-		} while (0)
-
-	#define BR_TODO(msg) \
-		do { \
-			br::errlnfmt("[{}:{}] todo: {}!", __FILE__, BR_STR(__LINE__), (msg)); \
+			std::puts(BR_TRACE "unimplemented!"); \
+			br::exit(br::EXIT_FAILURE); \
 		} while (0)
 
 	#ifndef BR_DISABLE_ASSERT
-		#define BR_DEBUG_RUN(expr) \
-			do { \
-				( (expr) ); \
-			} while (0)
-
 		#define BR_UNREACHABLE() \
 			do { \
-				br::halt("[{}:{}] unreachable!", __FILE__, BR_STR(__LINE__)); \
+				std::puts(BR_TRACE "unreachable!"); \
+				br::exit(br::EXIT_FAILURE); \
 			} while (0)
 
 		#define BR_ASSERT(cond) \
 			do { \
 				if (not (cond)) { \
-					br::halt("[{}:{}] assertion failed: '{}'!", __FILE__, BR_STR(__LINE__), BR_STR(cond)); \
+					std::puts(BR_TRACE "assertion failed: '" BR_STR(cond) "'!"); \
+					br::exit(br::EXIT_FAILURE); \
 				} \
 			} while (0)
 
 		#define BR_STATIC_ASSERT(cond) \
 			do { \
-				static_assert(cond, "[" __FILE__ ":" BR_STR(__LINE__) "] assertion failed: '" #cond "'!"); \
+				static_assert(cond, BR_TRACE "assertion failed: '" #cond "'!"); \
 			} while (0)
 
-
-		// We call this function in order to evaluate `expr` only once in case
-		// it has side effects. If we used a macro for this, it would be evaluated
-		// twice. Once for printing the result and once for returning it.
-		namespace detail {
-			template <typename T>
-			decltype(auto) print_debug_impl(const char* file, const char* line, const char* expr_s, T&& expr) {
-				br::printlnfmt("[{}:{}] {} = {}", file, line, expr_s, expr);
-				return expr;
-			}
-		}
-
-		#define BR_DEBUG(expr) \
-			( br::detail::print_debug_impl(__FILE__, BR_STR(__LINE__), BR_STR(expr), (expr)) )
-
 	#else
-		#define BR_DEBUG_RUN(expr) do {} while (0)
 		#define BR_UNREACHABLE() do {} while (0)
 		#define BR_ASSERT(cond) do {} while (0)
-		#define BR_STATIC_ASSERT(cond, msg) do {} while (0)
-		#define BR_DEBUG(expr) ( (expr) )
+		#define BR_STATIC_ASSERT(cond) do {} while (0)
 	#endif
 
 }
