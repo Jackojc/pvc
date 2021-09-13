3da1d7b6e8e7e1df610bb4432195e9a33b0d5340
Jack Clarke <jackojc@gmail.com>
2021-09-13 18:16:13 +0100

    Branchless functions and container helpers
    Added some branchless functions for:
    - conditionals (acts like a ternary)
    - abs (absolute value of number)
    - min/max
    
    Also added some container function helpers including:
    - length
    - at (access value at index)
    - set (set index to value)
    - front/back (access first and last elements)
    
    Other functions:
    - any (variadic function to check if _any_ arguments are true)
    - all (variadic function to check if _all_ arguments are true)

--- src/lib/misc.hpp
+++ src/lib/misc.hpp
@@ -13,6 +13,31 @@ namespace br {
 			((a - b) * (a > b));   // a > b => a - b
 	}
 
+
+	// Branchless ternary.
+	template <typename T>
+	auto condition(bool cond, T a, T b) {
+		return (cond * a) + (!cond * b);
+	}
+
+
+	// Branchless absolute function.
+	template <typename T> constexpr auto abs(T v) {
+		return v * ( (v > 0) - (v < 0) );
+	}
+
+
+	// Branchless min & max.
+	template <typename T> constexpr auto min(T a, T b) {
+		return condition(a < b, a, b);
+	}
+
+	template <typename T> constexpr auto max(T a, T b) {
+		return condition(a > b, a, b);
+	}
+
+
+	// Count the number of digits in an integer (radix 10).
 	template <typename T>
 	constexpr size_t count_digits(T x) {
 		size_t count = 0;
@@ -24,6 +49,49 @@ namespace br {
 		} while (x != 0);
 
 		return count;
+	}
+
+
+	// Get length of static array.
+	template <typename T, size_t N>
+	constexpr size_t length(T(&)[N]) {
+		return N;
+	}
+
+
+	// Check if any arguments are equal.
+	template <typename T, typename... Ts>
+	constexpr bool any(T&& first, Ts&&... rest) {
+		return ((first == rest) or ...);
+	}
+
+	// Check if all arguments are equal.
+	template <typename T, typename... Ts>
+	constexpr bool all(T&& first, Ts&&... rest) {
+		return ((first == rest) and ...);
+	}
+
+
+
+	// Read/write data at specified index.
+	template <typename T> constexpr auto at(T v, index_t i) {
+		return *data(v, i);
+	}
+
+	template <typename T> constexpr auto set(T v, index_t i, T x) {
+		*data(v, i) = x;
+		return v;
+	}
+
+
+
+	// Access the front and back of a container with random access.
+	template <typename T> constexpr auto front(T v) {
+		return at(v, 0);
+	}
+
+	template <typename T> constexpr auto back(T v) {
+		return at(v, length(v) - 1);
 	}
 
 }
